5bf08621eb019506e3c3457dfd049dc54547b3cd
ericmassol <massoleric07@gmail.com>
2024-12-03 10:37:20 +0100

    corr

--- Partie Software/code_test.ino
+++ Partie Software/code_test.ino
@@ -1,19 +1,21 @@
+#include <Arduino.h>
+
 // Définir la broche de la LED interne sur GPIO 22
-const int LED_BUILTIN = 22;
+const int LED_test = 22;
 
 void setup()
 {
   // Initialiser la broche de la LED comme sortie
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_test, OUTPUT);
 }
 
 void loop()
 {
   // Allumer la LED
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_test, HIGH);
   delay(1000); // Attendre 1 seconde
 
   // Éteindre la LED
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LED_test, LOW);
   delay(1000); // Attendre 1 seconde
 }
